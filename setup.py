21bd169074157b9c8bcba30b8c566a7be7242e49
RomeoDespres <Romeo.Despres@warnermusic.com>
2023-08-01 15:00:47 +0200

    Add base repo config

---
+++ setup.py
@@ -0,0 +1,25 @@
+from pathlib import Path
+from setuptools import setup
+
+
+name = "lofi"
+here = Path(__file__).absolute().parent
+requirements_path = here / "requirements.txt"
+dev_requirements_path = here / "requirements_dev.txt"
+
+
+def get_requirements(path: Path) -> list[str]:
+    return path.read_text().splitlines()
+
+
+setup(
+    author="Roméo Després",
+    maintainer="RomeoDespres",
+    name=name,
+    version="1.0.0",
+    packages=["lofi"],
+    install_requires=get_requirements(requirements_path),
+    extras_require={"dev": get_requirements(dev_requirements_path)},
+    include_package_data=True,
+    entry_points={"console_scripts": [f"{name} = {name}.cli:main"]},
+)
